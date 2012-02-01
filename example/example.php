3a70485abcf225413146d3c380e8f649a7cba7ed
ryan kibler <ecto01@gmail.com>
2012-02-01 11:06:31 -0800

    created example.php for implementation instead of using src

---
+++ example/example.php
@@ -0,0 +1,15 @@
+<?php
+
+require '../lastfm.php';
+
+$topArtists = getTopArtists();
+echo "<b>".$topArtists['username']."'s</b> most played artists<br /><br />\n";
+
+foreach($topArtists['artists'] as $artist)
+{
+  echo "<image src=\"".$artist['image']."\" /><br />\n";
+  echo "<a href=\"".$artist['link']."\"><b>".$artist['name']."</b></a><br />\n";
+  echo "plays: <b>".$artist['plays']."</b><br /><br /><br />\n\n";
+}
+
+?>
--- lastfm.php
+++ lastfm.php
@@ -29,14 +29,4 @@ function getTopArtists($user = LASTFM_USER)
   return $data;
 }
 
-
-$topArtists = getTopArtists();
-echo "<b>".$topArtists['username']."'s</b> most played artists<br /><br />\n";
-
-foreach($topArtists['artists'] as $artist)
-{
-  echo "<image src=\"".$artist['image']."\" /><br />\n";
-  echo "<a href=\"".$artist['link']."\"><b>".$artist['name']."</b></a><br />\n";
-  echo "plays: <b>".$artist['plays']."</b><br /><br /><br />\n\n";
-}
-
+?>
